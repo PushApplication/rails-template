09e23adaa4bcd435690ab911de3825b78b34d61b
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2016-04-18 04:20:39 +0200

    add rspec setup

--- default.rb
+++ default.rb
@@ -16,6 +16,26 @@ EOF
 end
 
 ###################
+# SMACSS
+###################
+
+run <<-EOF.gsub("\n", ' ').gsub(/\s{2,}/, ' ')
+cp -r #{dir_name}/smacss/base
+      #{dir_name}/smacss/fonts
+      #{dir_name}/smacss/layout
+      #{dir_name}/smacss/modules
+      #{dir_name}/smacss/style.scss
+      #{dir_name}/smacss/tools
+      app/assets/stylesheets
+EOF
+
+########################
+# Bootstrap & AngularJS
+########################
+
+run "cp #{dir_name}/config/application.html.erb app/views/layouts/"
+
+###################
 # Gemfile
 ###################
 
@@ -36,15 +56,15 @@ gem_group :test do
   gem 'mechanize', '2.7.4'
 end
 
-###################
-# SMACSS
-###################
-run <<-EOF.gsub("\n", ' ').gsub(/\s{2,}/, ' ')
-cp -r #{dir_name}/smacss/base
-      #{dir_name}/smacss/fonts
-      #{dir_name}/smacss/layout
-      #{dir_name}/smacss/modules
-      #{dir_name}/smacss/style.scss
-      #{dir_name}/smacss/tools
-      app/assets/stylesheets
-EOF
+run "bundle install"
+
+############
+# Rspec
+############
+
+generate "rspec:install"
+
+run "mkdir spec/controllers spec/factories spec/helpers spec/models spec/routing spec/views"
+
+run "cp #{dir_name}/config/spec/rails_helper.rb spec/"
+run "cp #{dir_name}/config/spec/spec_helper.rb  spec/"
